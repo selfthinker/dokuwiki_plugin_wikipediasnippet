d53d853d8027a36f435163022ec130bb9fac6265
Takuro Onoue <onoue@showway.biz>
2012-08-14 13:59:35 +0900

    fix Japanese language file

--- lang/ja/lang.php
+++ lang/ja/lang.php
@@ -3,5 +3,5 @@
  * Japanese language file for the WikipediaSnippet plugin
   */
 
-  $lang['permalink'] = '固定リンク';
+  $lang['permalink'] = '固定リンク(parmalink)';
   $lang['from']      = '<a href="%s">ウィキペディア</a>からの抜粋';
--- lang/ja/settings.php
+++ lang/ja/settings.php
@@ -3,5 +3,5 @@
  * Japanese language file for the config of the WikipediaSnippet plugin
  */
 
-$lang['includeTables']    = "Include tables";
-$lang['includeImages']    = "Include images";
+$lang['includeTables']    = "テーブルを含める";
+$lang['includeImages']    = "画像を含める";
