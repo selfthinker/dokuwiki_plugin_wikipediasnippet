3de0f28f34fc88be1cf4cc11a8464de6ceee4c8c
Tim Weinhold <tim.weinhold@gmail.com>
2013-08-15 10:35:05 +0200

    translation update

---
+++ lang/de/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Tim Weinhold <tim.weinhold@gmail.com>
+ */
+$lang['snippetLength']         = 'Anzahl der anzuzeigenden Sätze je Auszug; Leer oder \'0\' zeigt die ganze Einleitung an';
+$lang['useHtml']               = 'HTML-Formatierung im Auszug anzeigen? (Andernfalls wird nur unformatierter Text angezeigt)';
