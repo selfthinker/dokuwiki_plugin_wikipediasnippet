5c3359b5b40f9d653d7b55e43daa5ae236ef4f1f
Aleksandr Selivanov <alexgearbox@gmail.com>
2013-08-19 08:45:04 +0200

    translation update

---
+++ lang/ru/lang.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Aleksandr Selivanov <alexgearbox@gmail.com>
+ */
+$lang['from']                  = 'Фрагмент из <a href="%s">Википедии</a>';
