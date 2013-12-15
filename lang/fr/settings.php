427a2579e5311e3d88ea8e539bcfc391ddac8531
Nicolas Friedli <nicolas@theologique.ch>
2013-12-15 18:50:16 +0100

    translation update

---
+++ lang/fr/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Nicolas Friedli <nicolas@theologique.ch>
+ */
+$lang['snippetLength']         = 'Noombre de phrases à afficher dans le snippet. Laissez le champ vide ou affichez 0 (zéro) pour afficher toute l\'introduction';
+$lang['useHtml']               = 'Afficher le formattage HTML dans le snippet ? (sinon, n\'affiche que le texte non formatté)';
