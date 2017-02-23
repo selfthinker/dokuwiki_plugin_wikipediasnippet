1ec77ed86a47b79e27d5db6d50a6de2cf2919a00
Simon DELAGE <sdelage@gmail.com>
2017-02-23 07:20:50 +0100

    Minor translation update
    Fixed a typo and improved text

--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -5,5 +5,5 @@
  * 
  * @author Nicolas Friedli <nicolas@theologique.ch>
  */
-$lang['snippetLength']         = 'Noombre de phrases à afficher dans le snippet. Laissez le champ vide ou affichez 0 (zéro) pour afficher toute l\'introduction';
+$lang['snippetLength']         = 'Nombre de phrases à afficher dans le snippet. Laissez le champ vide ou indiquez 0 (zéro) pour afficher toute l\'introduction';
 $lang['useHtml']               = 'Afficher le formattage HTML dans le snippet ? (sinon, n\'affiche que le texte non formatté)';
