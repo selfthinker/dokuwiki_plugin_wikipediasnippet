1f6ca0c3d009806a824bff9ce78a45d814ca8c4b
Alexandre Belchior <alexbelchior@gmail.com>
2018-07-05 21:25:36 +0200

    translation update

---
+++ lang/pt-br/lang.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Alexandre Belchior <alexbelchior@gmail.com>
+ */
+$lang['from']                  = 'Snippet da <a href="%s">Wikipédia</a>';
